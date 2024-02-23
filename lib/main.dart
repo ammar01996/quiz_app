742dc9ba277ea0ea4a57241d282d0619d89b68f4
Ammar Namliwala <ammar01996@gmail.com>
2024-02-23 14:00:40 +0530

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 139, 93, 218),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset('assets/images/quiz-logo.png', width: 200),
-              const SizedBox(height: 20),
-              Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    fontSize: 30),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 139, 93, 218),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', width: 200),
+            const SizedBox(height: 20),
+            Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary, fontSize: 30),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                side: const BorderSide(color: Colors.white),
               ),
-              const SizedBox(height: 20),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  side: const BorderSide(color: Colors.white),
-                ),
-                onPressed: () {},
-                child: const Text('Start Quiz'),
-              ),
-            ],
-          ),
+              onPressed: () {},
+              child: const Text('Start Quiz'),
+            ),
+          ],
         ),
       ),
     );
